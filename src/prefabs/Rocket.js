01a0bebb3de22bdeb2dae2f26772248b289c16e6
Aubrey_Javachip <aschelba@ucsc.edu>
2021-04-11 00:00:48 -0700

    Collisions Implemented

--- src/prefabs/Rocket.js
+++ src/prefabs/Rocket.js
@@ -14,9 +14,28 @@ class Rocket extends Phaser.GameObjects.Sprite {
         if(!this.isFiring) {
             if(keyLEFT.isDown && this.x >= borderUISize + this.width) {
                 this.x -= this.moveSpeed;
-                }else if(keyRIGHT.isDown){
+                }else if(keyRIGHT.isDown && this.x <= game.config.width -borderUISize - this.width){
                     this.x += this.moveSpeed;
             }
         }
+        //fire button
+        if(Phaser.Input.Keyboard.JustDown(keyF)) {
+            this.isFiring = true;
+        }
+        //if fired move rocket up
+        if(this.isFiring && this.y >= borderUISize * 3 + borderPadding) {
+            this.y -= this.moveSpeed;
+        }
+        //reset on miss
+        if(this.y <= borderUISize * 3 + borderPadding) {
+            this.isFiring = false;
+            this.reset();
+        }
     }
-}
+
+    //reset rocket
+    reset() {
+        this.isFiring = false;
+        this.y = game.config.height - borderUISize - borderPadding;
+        }
+    }
